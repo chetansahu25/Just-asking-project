1e513edad3a0159101aa5b5250b1a7b53f319c05
Chetan <chetankumarsahu2525@gmail.com>
2025-02-15 18:29:47 +0530

    Simple Commit

--- src/app/api/sign-up/route.ts
+++ src/app/api/sign-up/route.ts
@@ -1,10 +1,11 @@
 import dbConnect from "@/lib/dbconnect";
 import UserModel from "@/model/User";
 import bcrypt from "bcryptjs";
-import { sendVerificationEmail } from "@/helpers/sendVerificationEmail";
+// import { sendVerificationEmail } from "@/helpers/sendVerificationEmail";
 
 export async function POST(request: Request) {
     await dbConnect();
+
     try {
         const { username, email, password } = await request.json();
         const existingUserVerifiedbyUsername = await UserModel.findOne({
